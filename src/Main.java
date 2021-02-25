75377bf27e13a5ad451a9e304f87f5863b0f7007
Paul Duschek <Paul.Duschek@HTL-Wels.at>
2021-02-25 11:36:11 +0100

    fully implemented class Main.java

--- src/Main.java
+++ src/Main.java
@@ -13,14 +13,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("ControllerView/sample.fxml"));
+        primaryStage.setTitle("Duschek - Aufgabe14 Adressbuch");
+        primaryStage.setScene(new Scene(root, 591, 400));
         primaryStage.show();
     }
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
+
