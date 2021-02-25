dbbeda58b325e74e39fd3ebb613bd73bcfe64b17
Paul Duschek <Paul.Duschek@HTL-Wels.at>
2021-02-25 12:03:08 +0100

    implemented method saveToCsv in Phonebook.java

--- src/ControllerView/Controller.java
+++ src/ControllerView/Controller.java
@@ -5,5 +5,26 @@
 
 package ControllerView;
 
+import Model.Phonebook;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    //FXML implementations
+    @FXML
+    private TextField name;
+    @FXML
+    private TextField adress;
+    @FXML
+    private TextField phone;
+    @FXML
+    private Label pageNr;
+
+    //variable definitions
+    private Phonebook book;
+    private int p;          //variable for pages later
+
+    //methods
 }
--- src/Model/Phonebook.java
+++ src/Model/Phonebook.java
@@ -5,5 +5,29 @@
 
 package Model;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.LinkedList;
+
 public class Phonebook {
+    private LinkedList<Person> phbook = new LinkedList<Person>();
+
+    public void saveToCsv()          //Method to save the file to phonebook.csv
+    {
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter("phonebook.csv")))
+        {
+            for(int i = 0; i < phbook.size(); i++)
+            {
+                bw.write(phbook.get(i).toString());
+                bw.newLine();
+            }
+            System.out.printf("File saved succesfully! %n");
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    
 }
