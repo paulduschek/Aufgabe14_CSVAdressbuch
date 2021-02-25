63e5e348901e4e951ece8cb66e285bc1bb29ef91
Paul Duschek <Paul.Duschek@HTL-Wels.at>
2021-02-25 11:28:48 +0100

    fully implemented variables, constructors, getter for class Person.java

--- src/Model/Person.java
+++ src/Model/Person.java
@@ -6,4 +6,41 @@
 package Model;
 
 public class Person {
+    //variable defintions
+    private String name;
+    private String addr;
+    private String phoneNR;
+
+    //Constructor
+    public Person()
+    {
+        setAddr("-");
+        setName("-");
+        setPhoneNR("-");
+    }
+
+    public Person(String n, String a, String pNR) {
+        this.name = n;
+        this.addr = a;
+        this.phoneNR = pNR;
+    }
+
+    //methods
+    @Override
+    public String toString() {
+        return name + "," + addr + "," + phoneNR;
+    }
+
+    //setter because no getter are needed
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public void setPhoneNR(String phoneNR) {
+        this.phoneNR = phoneNR;
+    }
 }
